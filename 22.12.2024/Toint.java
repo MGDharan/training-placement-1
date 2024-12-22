c4da2a263bddd6f920b32976389d4aaa69d98a8b
Giridharan <151344363+MGDharan@users.noreply.github.com>
2024-12-22 19:14:42 +0530

    Update Toint.java

--- 22.12.2024/Toint.java
+++ 22.12.2024/Toint.java
@@ -1,25 +1,13 @@
 class Solution {
-    public int romanToInt(String s) {
-        Map<Character, Integer> m = new HashMap<>();
-        
-        m.put('I', 1);
-        m.put('V', 5);
-        m.put('X', 10);
-        m.put('L', 50);
-        m.put('C', 100);
-        m.put('D', 500);
-        m.put('M', 1000);
-        
-        int ans = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            if (i < s.length() - 1 && m.get(s.charAt(i)) < m.get(s.charAt(i + 1))) {
-                ans -= m.get(s.charAt(i));
-            } else {
-                ans += m.get(s.charAt(i));
-            }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast = head, slow = head;
+        for (int i = 0; i < n; i++) fast = fast.next;
+        if (fast == null) return head.next;
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
         }
-        
-        return ans;
+        slow.next = slow.next.next;
+        return head;
     }
 }
