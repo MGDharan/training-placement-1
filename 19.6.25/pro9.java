132d6df632c99288ff6492744f9c744bc1d32e23
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-19 19:35:46 +0530

    Create pro9.java

---
+++ 19.6.25/pro9.java
@@ -0,0 +1,11 @@
+public class Armstrong {
+    public static void main(String[] args) {
+        int num = 153, temp = num, sum = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            sum += digit * digit * digit;
+            num /= 10;
+        }
+        System.out.println(temp == sum ? "Armstrong" : "Not Armstrong");
+    }
+}
