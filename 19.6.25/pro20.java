5e90bdb94c5dd5313f5998abcb052f1642d27e69
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-19 19:40:58 +0530

    Create pro20.java

---
+++ 19.6.25/pro20.java
@@ -0,0 +1,7 @@
+public class TemperatureConversion {
+    public static void main(String[] args) {
+        double c = 36.6;
+        double f = (c * 9/5) + 32;
+        System.out.println("Fahrenheit: " + f);
+    }
+}
