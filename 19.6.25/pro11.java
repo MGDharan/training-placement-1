87e6fffeed9af898a2b5d05042c7c3538f5a5932
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-19 19:36:28 +0530

    Create pro11.java

---
+++ 19.6.25/pro11.java
@@ -0,0 +1,7 @@
+public class AreaCircle {
+    public static void main(String[] args) {
+        double r = 7.0;
+        double area = Math.PI * r * r;
+        System.out.println("Area: " + area);
+    }
+}
