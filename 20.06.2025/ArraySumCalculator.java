8c6fcb07c233674dd8811dda9d91c4a8a07f5a2f
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-20 19:39:10 +0530

    Upload ArraySumCalculator.java

---
+++ 20.06.2025/ArraySumCalculator.java
@@ -0,0 +1,15 @@
+public class ArraySumCalculator {
+    public static int calculateSum(int[] arr) {
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        int sum = calculateSum(numbers);
+        System.out.println("Sum of array elements: " + sum);
+    }
+}
