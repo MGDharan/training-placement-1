c6f3122f93c3c3a01d43f21497b621747a1f7bbc
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-22 19:10:17 +0530

    Upload FibonacciSequence.java

---
+++ 22.06.2025/FibonacciSequence.java
@@ -0,0 +1,23 @@
+public class FibonacciSequence {
+
+    public static void generateFibonacci(int n) {
+        if (n <= 0) {
+            return;
+        }
+        int a = 0, b = 1;
+        System.out.print(a + " " + b);
+        for (int i = 2; i < n; ++i) {
+            int next = a + b;
+            System.out.print(" " + next);
+            a = b;
+            b = next;
+        }
+        System.out.println();
+
+    }
+
+    public static void main(String[] args) {
+        int numTerms = 10;
+        generateFibonacci(numTerms);
+    }
+}
