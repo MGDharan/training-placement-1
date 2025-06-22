37c460b0a54f7a536cd8de43c0aad5632fab0d7b
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-22 19:17:55 +0530

    Upload LinearSearch.java

---
+++ 22.06.2025/LinearSearch.java
@@ -0,0 +1,20 @@
+public class LinearSearch {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+        int target = 23;
+        int index = linearSearch(arr, target);
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found.");
+        }
+    }
+}
