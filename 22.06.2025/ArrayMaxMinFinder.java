78256e7d3099c4b6534637d282689820d6e27791
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-22 19:17:51 +0530

    Upload ArrayMaxMinFinder.java

---
+++ 22.06.2025/ArrayMaxMinFinder.java
@@ -0,0 +1,19 @@
+public class ArrayMaxMinFinder {
+    public static int[] findMinMax(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return new int[0];
+        }
+        int min = arr[0];
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) min = arr[i];
+            if (arr[i] > max) max = arr[i];
+        }
+        return new int[]{min, max};
+    }
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 4, 1, 5, 9, 2, 6};
+        int[] minMax = findMinMax(arr);
+        System.out.println("Min: " + minMax[0] + ", Max: " + minMax[1]);
+    }
+}
