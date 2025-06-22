0457f9774b44b44df0956cd117406ed81e64a9a0
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-22 19:20:44 +0530

    Upload SimpleCalculator.java

---
+++ 22.06.2025/SimpleCalculator.java
@@ -0,0 +1,37 @@
+public class SimpleCalculator {
+
+    public static double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public static double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public static double divide(double num1, double num2) {
+        if (num2 == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return num1 / num2;
+    }
+
+    public static void main(String[] args) {
+        double num1 = 10;
+        double num2 = 5;
+
+        System.out.println("Addition: " + add(num1, num2));
+        System.out.println("Subtraction: " + subtract(num1, num2));
+        System.out.println("Multiplication: " + multiply(num1, num2));
+        System.out.println("Division: " + divide(num1, num2));
+
+        try{
+            System.out.println("Division by zero: " + divide(num1,0));
+        } catch (ArithmeticException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
