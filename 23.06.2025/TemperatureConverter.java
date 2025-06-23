cef614e9adc9e0495e90bce8a16d381d7df6d786
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-23 19:44:30 +0530

    Upload TemperatureConverter.java

---
+++ 23.06.2025/TemperatureConverter.java
@@ -0,0 +1,14 @@
+public class  TemperatureConverter{
+    public static double celsiusToFahrenheit(double celsius){
+        return (celsius * 9/5) + 32;
+    }
+    public static double fahrenheitToCelsius(double fahrenheit){
+        return (fahrenheit - 32) * 5/9;
+    }
+    public static void main(String[] args){
+        double celsius = 25;
+        double fahrenheit = 77;
+        System.out.println(celsius + " degrees Celsius is equal to " + celsiusToFahrenheit(celsius) + " degrees Fahrenheit.");
+        System.out.println(fahrenheit + " degrees Fahrenheit is equal to " + fahrenheitToCelsius(fahrenheit) + " degrees Celsius.");
+    }
+}
