4d0d876d2d24cfbde254372f6c5ca7adaf66e441
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-23 19:54:01 +0530

    Upload ContainerWithMostWater.java

---
+++ 23.06.2025/ContainerWithMostWater.java
@@ -0,0 +1,19 @@
+public class  ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            int width = right - left;
+            int h = Math.min(height[left], height[right]);
+            int area = width * h;
+            maxArea = Math.max(maxArea, area);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
