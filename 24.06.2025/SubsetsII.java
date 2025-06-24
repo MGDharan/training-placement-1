1d3991de409c4c32bf23314357e9678151d30ed5
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-24 19:57:09 +0530

    Upload SubsetsII.java

---
+++ 24.06.2025/SubsetsII.java
@@ -0,0 +1,17 @@
+public class  SubsetsII {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(result, new ArrayList<>(), nums, 0);
+        return result;
+    }
+    private void backtrack(List<List<Integer>> result, List<Integer> subset, int[] nums, int start) {
+        result.add(new ArrayList<>(subset));
+        for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i - 1]) continue;
+            subset.add(nums[i]);
+            backtrack(result, subset, nums, i + 1);
+            subset.remove(subset.size() - 1);
+        }
+    }
+}
