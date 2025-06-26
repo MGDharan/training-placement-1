e1e62d433cba575934086f1876798227de88e85f
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-26 20:18:36 +0530

    Create pro3.py

---
+++ 26.06.2025/pro3.py
@@ -0,0 +1,14 @@
+def memoize(fn):
+    cache = {}
+    def wrapper(*args):
+        if args in cache:
+            return cache[args]
+        result = fn(*args)
+        cache[args] = result
+        return result
+    return wrapper
+
+@memoize
+def fibonacci(n):
+    if n in (0, 1): return n
+    return fibonacci(n-1) + fibonacci(n-2)
