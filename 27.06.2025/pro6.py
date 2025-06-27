866fe3c670eed2c313dc7c8a9ccb751e54eb457d
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-27 23:18:07 +0530

    Create pro6.py

---
+++ 27.06.2025/pro6.py
@@ -0,0 +1,29 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        cur = self.root
+        for ch in word:
+            if ch not in cur.children:
+                cur.children[ch] = TrieNode()
+            cur = cur.children[ch]
+        cur.is_end = True
+
+    def search(self, word):
+        cur = self.root
+        for ch in word:
+            if ch not in cur.children:
+                return False
+            cur = cur.children[ch]
+        return cur.is_end
+
+trie = Trie()
+trie.insert("code")
+print(trie.search("code"))  # True
+print(trie.search("coder"))  # False
