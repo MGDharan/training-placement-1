58753bbf0a93569f416d5b64920a5c6689748b64
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-28 16:33:32 +0530

    Upload geometric_shapes.py

---
+++ 28.06.2025/geometric_shapes.py
@@ -0,0 +1,29 @@
+# Filename:  geometric_shapes.py
+import math
+
+class Shape:
+    def area(self):
+        raise NotImplementedError("Subclasses must implement this method.")
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius**2
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+class Triangle(Shape):
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return 0.5 * self.base * self.height
