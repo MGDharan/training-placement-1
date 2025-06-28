51047be7c7f90c1f8471d1527713f2d55bbbe2c6
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-06-28 16:32:29 +0530

    Upload complex_number_calculator.py

---
+++ 28.06.2025/complex_number_calculator.py
@@ -0,0 +1,19 @@
+# Filename:  complex_number_calculator.py
+class ComplexNumber:
+    def __init__(self, real, imag):
+        self.real = real
+        self.imag = imag
+
+    def __add__(self, other):
+        return ComplexNumber(self.real + other.real, self.imag + other.imag)
+
+    def __sub__(self, other):
+        return ComplexNumber(self.real - other.real, self.imag - other.imag)
+
+    def __mul__(self, other):
+        real_part = self.real * other.real - self.imag * other.imag
+        imag_part = self.real * other.imag + self.imag * other.real
+        return ComplexNumber(real_part, imag_part)
+
+    def __str__(self):
+        return f"{self.real} + {self.imag}i"
