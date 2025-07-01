fd1e486cc99ce0485b8ec7167ca834f6a6deb88b
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-07-01 21:30:06 +0530

    Upload timer.py

---
+++ 30.06.2025/timer.py
@@ -0,0 +1,11 @@
+# Filename:  timer.py
+import time
+def timeit(func):
+    """Decorator to measure the execution time of a function."""
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Execution time: {end_time - start_time:.4f} seconds")
+        return result
+    return wrapper
